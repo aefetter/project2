2c1f1996ae0a0e8e8e3cddeac851b521119e0179
Anna <aefetter@unc.edu>
2024-03-25 19:52:45 -0400

    current version of assignment

--- js/myscript.js
+++ js/myscript.js
@@ -7,3 +7,21 @@ function myMenu() {
       x.style.display = "block";
     }
   }
+
+// typewriter animation for Chanel page
+var i = 0;
+var txt = '"Fashion is not something that exists in dresses only. Fashion is in the sky, in the street, fashion has to do with ideas, the way we live, what is happening."'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("chanel-quote").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+// Call the typeWriter function after page loads
+window.onload = function() {
+  typeWriter();
+};
